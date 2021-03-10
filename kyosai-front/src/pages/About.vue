8ce3f9f7e0b50a0800d2aac684ef0033f11c0c09
Michael <michaelbecquer@yahoo.fr>
2021-03-10 19:00:50 +0100

    setup axios

--- kyosai-front/src/pages/About.vue
+++ kyosai-front/src/pages/About.vue
@@ -2,17 +2,34 @@
   <Layout>
     <h1>About us</h1>
 
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error doloremque omnis animi, eligendi magni a voluptatum, vitae, consequuntur rerum illum odit fugit assumenda rem dolores inventore iste reprehenderit maxime! Iusto.</p>
+    <p>{{content}}</p>
   </Layout>
 </template>
 
 <script>
+const axios = require('axios');
 
 export default {
 
   metaInfo: {
     title: 'About us',
   
+  },
+  created(){
+  
+axios.get('https://public-api.wordpress.com/rest/v1.2/sites/assokyosai.wordpress.com')
+  .then( (response)=> {
+    // handle success
+    console.log(response);
+    this.content = response.data;
+  })
+  
+  },
+
+  data(){
+    return {
+      content:"loading"
+    }
   }
 }
 </script>
