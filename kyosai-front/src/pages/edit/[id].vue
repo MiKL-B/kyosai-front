d978348c1b1128ecd080b189cbe276eb0c563626
Michael <michaelbecquer@yahoo.fr>
2021-04-08 14:16:43 +0200

    foreach category wip

--- kyosai-front/src/pages/edit/[id].vue
+++ kyosai-front/src/pages/edit/[id].vue
@@ -14,7 +14,8 @@
       <!--info sur le produit-->
       <div class="flex flex-col">
         <p class="font-black capitalize text-2xl">{{ editContent.nom }}</p>
-        <p>
+        <p v-for="content in editContent" :key="content.id" :content="content">
+     
           {{
             editContent.categories &&
               getCategoryById(
@@ -23,7 +24,7 @@
                 )
               ) &&
               getCategoryById(
-                editContent.categories[0].substring(
+                editContent.categories[2].substring(
                   editContent.categories[0].lastIndexOf("/") + 1
                 )
               ).label
@@ -44,9 +45,10 @@
             type="text"
             placeholder="Saisir nouveau nom"
           />
-        <!--  <select
+       <select
             v-model="category"
             v-if="categoryContentList.length > 0"
+            multiple
             class="p-5 text-lg border-2 rounded-lg focus:outline-none focus:ring-2 focus:ring-pink-400 focus:text-pink-400"
           >
             <option value="" selected>Choisissez une catégorie</option>
@@ -71,7 +73,7 @@
             type="file"
             placeholder="Image"
             accept="image/*"
-          />-->
+          />
           <button
             class="p-3 text-lg flex my-2 mx-auto bg-gray-600 hover:bg-pink-400 rounded-lg text-white uppercase"
             type="submit"
@@ -87,6 +89,14 @@
 <script>
 import dayjs from "dayjs";
 const axios = require("axios");
+//get base64 from image
+const getBase64 = (file) =>
+  new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => resolve(reader.result);
+    reader.onerror = (error) => reject(error);
+  });
 export default {
   metaInfo: {
     title: "Edition de produit",
@@ -97,7 +107,7 @@ export default {
   data() {
     return {
       success: "",
-      editContent: "",
+      editContent: [],
       categoryContentList: [],
       name: "",
       category: [],
@@ -111,9 +121,9 @@ export default {
               const { id } = this.$route.params;
       axios.post(`http://127.0.0.1:8000/api/admin/edit/${id}`, {
           name_produit: this.name,
-          //prix_produit: this.prix,
-          //image_produit: this.image,
-         // category_produit: this.category,
+          prix_produit: this.prix,
+          image_produit: this.image,
+          category_produit: this.category,
         })
         .then(function(response) {
           console.log(response);
@@ -144,6 +154,15 @@ export default {
         this.categoryContentList.find((category) => category.id == id)
       );
     },
+   
+allMyCategories(){
+this.editContent.forEach((value, index) => {
+   
+    console.log(value);
+    console.log(index);
+});
+}
+
   },
   computed: {},
   created() {
